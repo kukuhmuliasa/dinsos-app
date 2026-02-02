3ac9caf5bf2d221bda55710e456bda0bc8310a21
Rizkichy19 <rizkicahyaputra@gmail.com>
2026-02-02 15:18:41 +0700

    Restore desain dan fix upload dokumen

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-<<<<<<< Updated upstream
-    protected $fillable = ['title', 'file_path', 'category'];
-=======
   protected $fillable = [
     'title',
     'category',
     'file',];
->>>>>>> Stashed changes
 }
